ec454adea890ab321d08a96179351ae4c39ab362
Tom Peterka <tpeterka@mcs.anl.gov>
2014-01-31 18:56:59 +0000

    ported to BG/Q
    --HG--
    extra : convert_revision : svn%3A51b2a480-81d2-489e-acae-26a3fc869840/trunk%4021

--- user_defs.mk
+++ user_defs.mk
@@ -17,29 +17,31 @@
 
 # 1. Set your architecture here
 
-ARCH = MAC_OSX
+#ARCH = MAC_OSX
 #ARCH = LINUX
 #ARCH = BGP
-#ARCH = BGQ
+ARCH = BGQ
 
 # 2. Set your choice of computational geometry library here (QHULL or CGAL)
 
-CONV = QHULL
-#CONV = CGAL
+#CONV = QHULL
+CONV = CGAL
 
 # 3. Set your dependency paths here
 
-DIY_INC = -I$(HOME)/diy/include
+DIY_INC = -I$(HOME)/diy-gcc/include
 QHULL_INC =-I$(HOME)/software/qhull-2011.2/src/libqhull
-CGAL_INC =-I/opt/local/include
+CGAL_INC =-I/$(HOME)/software/CGAL-4.3/include -I/soft/libraries/boost/current/cnk-gcc/current/include -I/usr/include
 PNETCDF_INC = -I$(HOME)/software/parallel-netcdf-1.3.0/include
-DIY_LIB = -L$(HOME)/diy/lib -ldiy
+DIY_LIB = -L$(HOME)/diy-gcc/lib -ldiy
 QHULL_LIB = -L$(HOME)/software/qhull-2011.2/lib -lqhullstatic
-CGAL_LIB = -L/opt/local/lib -lgmp -lCGAL
+CGAL_LIB = -L$(HOME)/software/CGAL-4.3/lib -L$(HOME)/software/gmp-5.1.3/lib -lgmp -lCGAL -L$/soft/libraries/boost/current/cnk-gcc/current/lib
 PNETCDF_LIB = -L$(HOME)/software/parallel-netcdf-1.3.0/lib -lpnetcdf
 
 # 4. Set your build options here
 
+CC=/soft/compilers/wrappers/gcc/mpicxx
+CXX=/soft/compilers/wrappers/gcc/mpicxx
 TIMING = -DTIMING
 
 #----------------------------------------------------------------------------
@@ -59,6 +61,8 @@ endif
 ifeq ($(CONV), CGAL) # cgal version
 
 CGAL_CCFLAGS=-frounding-math -DCGAL_DISABLE_ROUNDING_MATH_CHECK \
+	     -DTESS_CGAL_ALLOW_SPATIAL_SOR # should be based on compiler check
+#CGAL_CCFLAGS=-DCGAL_DISABLE_ROUNDING_MATH_CHECK \
 	     -DTESS_CGAL_ALLOW_SPATIAL_SOR # should be based on compiler check
 DELAUNAY_INC=${CGAL_INC}
 DELAUNAY_LIB=${CGAL_LIB}
