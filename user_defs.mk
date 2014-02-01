7fe97ea257c35d798e178b5986c83e21b3f1472e
Tom Peterka <tpeterka@mcs.anl.gov>
2014-02-01 18:07:56 +0000

    updated user_defs.mk to make it easier to switch between machines
    --HG--
    extra : convert_revision : svn%3A51b2a480-81d2-489e-acae-26a3fc869840/trunk%4023

--- user_defs.mk
+++ user_defs.mk
@@ -17,31 +17,46 @@
 
 # 1. Set your architecture here
 
-#ARCH = MAC_OSX
+ARCH = MAC_OSX
 #ARCH = LINUX
 #ARCH = BGP
-ARCH = BGQ
+#ARCH = BGQ
 
 # 2. Set your choice of computational geometry library here (QHULL or CGAL)
 
-#CONV = QHULL
-CONV = CGAL
+CONV = QHULL
+#CONV = CGAL
 
-# 3. Set your dependency paths here
+# 3. Set your dependency paths and build options here
 
+ifeq ($(ARCH), BGQ) # BG/Q version
+
+CC=/soft/compilers/wrappers/gcc/mpicc
+CXX=/soft/compilers/wrappers/gcc/mpicxx
 DIY_INC = -I$(HOME)/diy-gcc/include
 QHULL_INC =-I$(HOME)/software/qhull-2011.2/src/libqhull
 CGAL_INC =-I/$(HOME)/software/CGAL-4.3/include -I/soft/libraries/boost/current/cnk-gcc/current/include -I/usr/include
-PNETCDF_INC = -I$(HOME)/software/parallel-netcdf-1.3.0/include
+PNETCDF_INC = -I$(HOME)/software/parallel-netcdf-1.3.0-gcc/include
 DIY_LIB = -L$(HOME)/diy-gcc/lib -ldiy
+QHULL_LIB = -L$(HOME)/software/qhull-2011.2/lib -lqhullstatic
+CGAL_LIB = -L$(HOME)/software/CGAL-4.3-install/lib -L$(HOME)/software/gmp-5.1.3/lib -lgmp -dynamic -lCGAL -L/soft/libraries/boost/current/cnk-gcc/current/lib -lboost_system-mt -lboost_thread-mt
+PNETCDF_LIB = -L$(HOME)/software/parallel-netcdf-1.3.0-gcc/lib -lpnetcdf
+
+else
+
+CC=mpicc
+CXX=mpicxx
+DIY_INC = -I$(HOME)/diy/include
+QHULL_INC =-I$(HOME)/software/qhull-2011.2/src/libqhull
+CGAL_INC =-I/$(HOME)/software/CGAL-4.3/include -I/soft/libraries/boost/current/cnk-gcc/current/include -I/usr/include
+PNETCDF_INC = -I$(HOME)/software/parallel-netcdf-1.3.0/include
+DIY_LIB = -L$(HOME)/diy/lib -ldiy
 QHULL_LIB = -L$(HOME)/software/qhull-2011.2/lib -lqhullstatic
 CGAL_LIB = -L$(HOME)/software/CGAL-4.3-install/lib -L$(HOME)/software/gmp-5.1.3/lib -lgmp -dynamic -lCGAL -L/soft/libraries/boost/current/cnk-gcc/current/lib -lboost_system-mt -lboost_thread-mt
 PNETCDF_LIB = -L$(HOME)/software/parallel-netcdf-1.3.0/lib -lpnetcdf
 
-# 4. Set your build options here
+endif
 
-CC=/soft/compilers/wrappers/gcc/mpicxx
-CXX=/soft/compilers/wrappers/gcc/mpicxx
 TIMING = -DTIMING
 
 #----------------------------------------------------------------------------
@@ -61,8 +76,6 @@ endif
 ifeq ($(CONV), CGAL) # cgal version
 
 CGAL_CCFLAGS=-frounding-math -DCGAL_DISABLE_ROUNDING_MATH_CHECK \
-	     -DTESS_CGAL_ALLOW_SPATIAL_SOR # should be based on compiler check
-#CGAL_CCFLAGS=-DCGAL_DISABLE_ROUNDING_MATH_CHECK \
 	     -DTESS_CGAL_ALLOW_SPATIAL_SOR # should be based on compiler check
 DELAUNAY_INC=${CGAL_INC}
 DELAUNAY_LIB=${CGAL_LIB}
