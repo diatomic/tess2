a2c3853a859353101cab2e9c1e33a85f4c90e8cd
Tom Peterka <tpeterka@mcs.anl.gov>
2014-05-07 01:17:14 +0000

    fixed manual makefile for Mac to build with CGAL correctly
    --HG--
    extra : convert_revision : svn%3A51b2a480-81d2-489e-acae-26a3fc869840/trunk%40143

--- user_defs.mk
+++ user_defs.mk
@@ -23,8 +23,8 @@ ARCH = MAC_OSX
 
 # 2. Set your choice of computational geometry library here (QHULL or CGAL)
 
-CONV = QHULL
-#CONV = CGAL
+#CONV = QHULL
+CONV = CGAL
 
 # 3. Set your dependency paths and build options here
 
@@ -36,7 +36,7 @@ DIY_INC = -I$(HOME)/diy-gcc/include
 QHULL_INC =-I$(HOME)/software/qhull-2011.2/src/libqhull
 CGAL_INC =-I/$(HOME)/software/CGAL-4.3/include -I/soft/libraries/boost/current/cnk-gcc/current/include -I/usr/include
 PNETCDF_INC = -I$(HOME)/software/parallel-netcdf-1.3.0-gcc/include
-DIY_LIB = -L$(HOME)/diy-gcc/lib -ldiy
+DIY_LIB = $(HOME)/diy-gcc/lib/libdiy.a
 QHULL_LIB = -L$(HOME)/software/qhull-2011.2/lib -lqhullstatic
 CGAL_LIB = -L$(HOME)/software/CGAL-4.3-install/lib -L$(HOME)/software/gmp-5.1.3/lib -lgmp -dynamic -lCGAL -L/soft/libraries/boost/current/cnk-gcc/current/lib -lboost_system-mt -lboost_thread-mt
 PNETCDF_LIB = -L$(HOME)/software/parallel-netcdf-1.3.0-gcc/lib -lpnetcdf
@@ -47,11 +47,13 @@ CC=mpicc
 CXX=mpicxx
 DIY_INC = -I$(HOME)/diy/include
 QHULL_INC =-I$(HOME)/software/qhull-2011.2/src/libqhull
-CGAL_INC =-I/$(HOME)/software/CGAL-4.3/include -I/soft/libraries/boost/current/cnk-gcc/current/include -I/usr/include
+CGAL_INC =-I/opt/local/include
 PNETCDF_INC = -I$(HOME)/software/parallel-netcdf-1.3.0/include
-DIY_LIB = -L$(HOME)/diy/lib -ldiy
+DIY_LIB = $(HOME)/diy/lib/libdiy.a
 QHULL_LIB = -L$(HOME)/software/qhull-2011.2/lib -lqhullstatic
-CGAL_LIB = -L$(HOME)/software/CGAL-4.3-install/lib -L$(HOME)/software/gmp-5.1.3/lib -lgmp -dynamic -lCGAL -L/soft/libraries/boost/current/cnk-gcc/current/lib -lboost_system-mt -lboost_thread-mt
+CGAL_LIB = -L/opt/local/lib -lgmp -lCGAL  \
+	$(HOME)/software/mpich-3.0.4-install/lib/libpmpich.a \
+	$(HOME)/software/mpich-3.0.4-install/lib/libmpich.a
 PNETCDF_LIB = -L$(HOME)/software/parallel-netcdf-1.3.0/lib -lpnetcdf
 
 endif
@@ -75,8 +77,10 @@ endif
 
 ifeq ($(CONV), CGAL) # cgal version
 
-CGAL_CCFLAGS=-frounding-math -DCGAL_DISABLE_ROUNDING_MATH_CHECK \
+#CGAL_CCFLAGS=-frounding-math -DCGAL_DISABLE_ROUNDING_MATH_CHECK \
 	     -DTESS_CGAL_ALLOW_SPATIAL_SOR # should be based on compiler check
+CGAL_CCFLAGS=-frounding-math -DCGAL_USE_MPFR -DCGAL_USE_GMP \
+	     -DTESS_CGAL_ALLOW_SPATIAL_SOR -pipe -Os -arch x86_64 -fno-strict-aliasing -O3
 DELAUNAY_INC=${CGAL_INC}
 DELAUNAY_LIB=${CGAL_LIB}
 DELAUNAY_CCFLAGS=${CGAL_CCFLAGS}
